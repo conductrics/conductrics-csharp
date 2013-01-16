96f6787e96e4d1707b7faae66c158900bd7ee81b
Jesse Dailey <jesse.dailey@gmail.com>
2013-01-16 16:25:34 -0500

    initial commit

---
+++ Conductrics/Client.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+
+namespace Conductrics {
+	public class Client {
+		string key;
+		string owner;
+		string baseUrl;
+		public Client(string ownerCode) {
+			this.baseUrl = "http://api.conductrics.com/";
+			this.owner = ownerCode;
+		}
+		public Client(string url, string ownerCode) {
+			this.baseUrl = url;
+			this.owner = ownerCode;
+		}
+		public Client apiKey( string key ) {
+			this.key = key;
+			return this;
+		}
+		public string GetDecision( string agentCode, string[] choices ) {
+			return GetDecision(agentCode, choices, "");
+		}
+		public string GetDecision(string agentCode, string[] choices, string session) {
+			// http://api.conductrics.com/owner_sxvgyHUlj/agent-1/decision/decision-1/4?apikey=api-nQyALpnyPsZHQrVbvtOhZpYz
+			string url = baseUrl + "/" + owner + "/" + agentCode + "/decision/decision-1/" + choices.Length;
+			var http = (HttpWebRequest)WebRequest.Create(url);
+			http.Headers.Add("mpath-apikey", this.key);
+			if( session != null && session.Length > 0 )
+				http.Headers.Add("mpath-session", session);
+			var content = new StreamReader(http.GetResponse().GetResponseStream()).ReadToEnd();
+			dynamic result = JsonValue.Parse(content);
+			Debug.Print(result);
+		}
+	}
+}
