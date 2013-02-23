332d04d0990207fa95b53015ae8011080768a0ef
Jesse Dailey <jesse.dailey@gmail.com>
2013-02-23 02:46:56 -0500

    add Agent::Reward

--- Conductrics/Agent.cs
+++ Conductrics/Agent.cs
@@ -6,6 +6,7 @@ using System.Net;
 using System.Diagnostics;
 using System.IO;
 using System.Web.Script.Serialization;
+using System.Net.Cache;
 
 namespace Conductrics {
 	public static class API {
@@ -19,26 +20,45 @@ namespace Conductrics {
 	}
 	public class Agent {
 		public string name;
-		public Agent(string name) {
+		public Agent(string name ) {
 			this.name = name;
 		}
-		public string Decide( string[] choices ) {
-			return Decide(choices, "");
+		private HttpWebRequest mpathRequest( string url, string session ) {
+			var http = (HttpWebRequest)WebRequest.Create(url);
+			http.CachePolicy = new RequestCachePolicy(RequestCacheLevel.BypassCache);
+			http.Pipelined = true;
+			http.KeepAlive = false;
+			http.Timeout = 1000;
+			http.Headers.Add("x-mpath-apikey", API.Key);
+			if( session != null && session.Length > 0 )
+				http.Headers.Add("x-mpath-session", session);
+			return http;
 		}
-		public string Decide( string[] choices, string session) {
+		public void Reward( string session, string goalCode = "goal-1", double value = 1.0 ) {
+			string url = API.BaseUrl + "/"
+				+ API.Owner + "/"
+				+ this.name + "/goal/"
+				+ goalCode
+				+ "?apikey=" + API.Key
+				+ "&reward=" + String.Format("{0:0.00}", value);
+			var http = mpathRequest(url, session);
+			try {
+				var response = http.GetResponse();
+				response.Close();
+			} catch( Exception e ) {
+				Debug.Print(e.ToString());
+			}
+
+		}
+		public T Decide<T>( string session, params T[] choices) {
 			// http://api.conductrics.com/owner_sxvgyHUlj/agent-1/decision/decision-1/4?apikey=api-nQyALpnyPsZHQrVbvtOhZpYz
 			string url = API.BaseUrl + "/"
 				+ API.Owner + "/"
 				+ this.name + "/decision/"
 				+ choices.Length
 				+ "?apikey=" + API.Key;
-			var http = (HttpWebRequest)WebRequest.Create(url);
-			http.KeepAlive = false;
-			http.Timeout = 1000;
-			http.Headers.Add("x-mpath-apikey", API.Key);
-			if( session != null && session.Length > 0 )
-				http.Headers.Add("x-mpath-session", session);
 			Debug.Print("Headers added, beginning request... {0}", url);
+			var http = mpathRequest(url, session);
 			try {
 				var response = http.GetResponse();
 				Debug.Print("Have response.");
@@ -47,11 +67,13 @@ namespace Conductrics {
 				Debug.Print("Finished reading response.");
 				JavaScriptSerializer ser = new JavaScriptSerializer();
 				SimpleDecision d = ser.Deserialize<SimpleDecision>(content);
-				return d.decision;
+				int index = int.Parse(d.decision);
+				if( index > 0 && index < choices.Length )
+					return choices[index];
 			} catch( Exception e ) {
 				Debug.Print(e.ToString());
-				return choices[0];
 			}
+			return choices[0];
 		}
 	}
 }
